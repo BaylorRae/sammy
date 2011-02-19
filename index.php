a07d1e7a2c361bc728a0cd3ed2415f3ab7befa56
BaylorRae <baylorrae@gmail.com>
2011-02-18 18:40:14 -0600

    Added support for formats, like .json, .xml

--- index.php
+++ index.php
@@ -5,6 +5,26 @@ get('/', function() {
 	return 'Hello World!';
 });
 
+get('/users', function($sammy) {
+  
+  // example.com/users.json
+  $sammy->format('json', function() {
+      return json_encode(array('users' => array(
+          array(
+            'username' => 'bob',
+            'api_key' => '5829-akeK-Ai49'
+            ),
+          array(
+            'username' => 'sam',
+            'api_key' => '4938-Akeb-Le32'
+            )
+        )));
+  });
+  
+  if( !$sammy->format )
+    return 'Hello';
+});
+
 get('/hello/(.*?)', function($sammy) {
 	return 'Hello '.$sammy->segment(2);
 });
--- sammy.php
+++ sammy.php
@@ -8,127 +8,109 @@
  * @copyright	2010 Dan Horrigan
  */
 
-function get($route, $callback)
-{
+function get($route, $callback) {
 	Sammy::process($route, $callback, 'GET');
 }
 
-function post($route, $callback)
-{
+function post($route, $callback) {
 	Sammy::process($route, $callback, 'POST');
 }
 
-function put($route, $callback)
-{
+function put($route, $callback) {
 	Sammy::process($route, $callback, 'PUT');
 }
 
-function delete($route, $callback)
-{
+function delete($route, $callback) {
 	Sammy::process($route, $callback, 'DELETE');
 }
 
 class Sammy {
 	
 	public static $route_found = false;
-
-	public static function instance()
-	{
-		static $instance = null;
-		
-		if ($instance === null)
-		{
-			$instance = new Sammy;
-		}
-		
-		return $instance;
-	}
-
-	public static function run()
-	{
-		if ( ! static::$route_found)
-		{
-			echo 'Route not defined!';
-		}
-		
-		ob_end_flush();
-	}
-
-	public static function process($route, $callback, $type)
-	{
-		$sammy = static::instance();
-		if (static::$route_found || ( ! preg_match('@^'.$route.'$@uD', $sammy->uri) || $sammy->method != $type))
-		{
-			return false;
-		}
-		static::$route_found = true;
-		echo $callback($sammy);
-	}
-
+	
 	public $uri = '';
 
 	public $segments = '';
 
 	public $method = '';
 	
-	public function __construct()
-	{
+	public $format = '';
+
+	public static function instance() {
+		static $instance = null;
+		
+		if( $instance === null ) {
+			$instance = new Sammy;
+		}
+		
+		return $instance;
+	}
+
+	public static function run() {
+		if( !static::$route_found ) {
+			echo 'Route not defined!';
+		}
+		
+		ob_end_flush();
+	}
+
+	public static function process($route, $callback, $type) {
+		$sammy = static::instance();
+		if( static::$route_found || (!preg_match('@^'.$route.'(?:\.(\w+))?$@uD', $sammy->uri, $matches) || $sammy->method != $type) ) {
+			return false;
+		}
+		
+		$sammy->format = (!empty($matches[1])) ? $matches[1] : null;
+		
+		static::$route_found = true;
+		echo $callback($sammy);
+	}
+	
+	public function __construct() {
 		ob_start();
 		$this->uri = $this->get_uri();
 		$this->segments = explode('/', trim($this->uri, '/'));
 		$this->method = $this->get_method();
 	}
 
-	public function segment($num)
-	{
+	public function segment($num) {
 		return isset($this->segments[$num - 1]) ? $this->segments[$num - 1] : null;
 	}
 
-	protected function get_method()
-	{
+	protected function get_method() {
 		return isset($_SERVER['REQUEST_METHOD']) ? $_SERVER['REQUEST_METHOD'] : 'GET';
 	}
 	
-	protected function get_uri($prefix_slash = true)
-	{
-	    if (isset($_SERVER['PATH_INFO']))
-	    {
+	protected function get_uri($prefix_slash = true) {
+	    if( isset($_SERVER['PATH_INFO']) ) {
 	        $uri = $_SERVER['PATH_INFO'];
-	    }
-	    elseif (isset($_SERVER['REQUEST_URI']))
-	    {
+	    }elseif( isset($_SERVER['REQUEST_URI']) ) {
 	        $uri = $_SERVER['REQUEST_URI'];
-	        if (strpos($uri, $_SERVER['SCRIPT_NAME']) === 0)
-	        {
+	        
+	        if( strpos($uri, $_SERVER['SCRIPT_NAME']) === 0 ) {
 	            $uri = substr($uri, strlen($_SERVER['SCRIPT_NAME']));
-	        }
-	        elseif (strpos($uri, dirname($_SERVER['SCRIPT_NAME'])) === 0)
-	        {
+	        }elseif( strpos($uri, dirname($_SERVER['SCRIPT_NAME'])) === 0 ) {
 	            $uri = substr($uri, strlen(dirname($_SERVER['SCRIPT_NAME'])));
 	        }
 
 	        // This section ensures that even on servers that require the URI to be in the query string (Nginx) a correct
 	        // URI is found, and also fixes the QUERY_STRING server var and $_GET array.
-	        if (strncmp($uri, '?/', 2) === 0)
-	        {
+	        if( strncmp($uri, '?/', 2) === 0 ) {
 	            $uri = substr($uri, 2);
 	        }
+	        
 	        $parts = preg_split('#\?#i', $uri, 2);
 	        $uri = $parts[0];
-	        if (isset($parts[1]))
-	        {
+	        
+	        if( isset($parts[1]) ) {
 	            $_SERVER['QUERY_STRING'] = $parts[1];
 	            parse_str($_SERVER['QUERY_STRING'], $_GET);
-	        }
-	        else
-	        {
+	        }else {
 	            $_SERVER['QUERY_STRING'] = '';
 	            $_GET = array();
 	        }
 	        $uri = parse_url($uri, PHP_URL_PATH);
-	    }
-	    else
-	    {
+	    }else {
 	        // Couldn't determine the URI, so just return false
 	        return false;
 	    }
@@ -137,6 +119,13 @@ class Sammy {
 	    return ($prefix_slash ? '/' : '').str_replace(array('//', '../'), '/', trim($uri, '/'));
 	}
 	
+	public function format($name, $callback) {
+	  $sammy = static::instance();
+	  if( !empty($sammy->format) && $name == $sammy->format )
+	    echo $callback($sammy);
+	  else
+	    return false;
+	}
 }
 
 $sammy = Sammy::instance();
